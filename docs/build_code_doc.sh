8331c94137f9d6209e4d7bf84e2f0efafbf84c90
JonatanEnes <jonatan.enes@udc.es>
2020-06-23 21:33:00 +0200

    Updated documentation with new build of source code

--- docs/build_code_doc.sh
+++ docs/build_code_doc.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-source /home/jonatan/development/BDWatchdog/set_pythonpath.sh
+source /home/jonatan/development/ServerlessContainers/set_pythonpath.sh
 pdoc3 --html --template-dir templates/ --force ../src -o code
 
 #pdoc3 --html --template-dir templates/ --force ../src/Guardian ../src/Rescaler -o code/
