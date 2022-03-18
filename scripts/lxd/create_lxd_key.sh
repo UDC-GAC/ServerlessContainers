b672ae0db757219bf3644ca9dce4f9c82241933f
JonatanEnes <jonatan.enes@udc.es>
2022-03-18 10:29:06 +0100

    LXD key creation is not automatic, without any user prompt

--- scripts/lxd/create_lxd_key.sh
+++ scripts/lxd/create_lxd_key.sh
@@ -4,5 +4,5 @@ export LXD_KEY_NAME="lxd-$(hostname)"
 scriptDir=$(dirname -- "$(readlink -f -- "$BASH_SOURCE")")
 source "${scriptDir}/../../set_pythonpath.sh"
 openssl genrsa -out ${SERVERLESS_PATH}/${LXD_KEY_NAME}.key 4096
-openssl req -new -key ${SERVERLESS_PATH}/${LXD_KEY_NAME}.key -out ${SERVERLESS_PATH}/${LXD_KEY_NAME}.csr
+openssl req -new -key ${SERVERLESS_PATH}/${LXD_KEY_NAME}.key -out ${SERVERLESS_PATH}/${LXD_KEY_NAME}.csr -subj "/C=ES/ST=A Coruna/L=A Coruna/O=UDC-FIC/CN="
 openssl x509 -req -days 3650 -in ${SERVERLESS_PATH}/${LXD_KEY_NAME}.csr -signkey ${SERVERLESS_PATH}/${LXD_KEY_NAME}.key -out ${SERVERLESS_PATH}/${LXD_KEY_NAME}.crt
