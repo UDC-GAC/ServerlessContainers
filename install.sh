ed308db3ce0a4d9b2d52f845d4e7620545fe50d0
Jonatan Enes <jonatan.enes@udc.es>
2023-10-28 21:41:35 +0200

    Update install.sh
    Fixes in install requirements

--- install.sh
+++ install.sh
@@ -1,4 +1,3 @@
 pip3 install -r requirements.txt
-apt install -y gunicorn
-apt install -y python3-coverage
-apt install -y python3-nose
+apt install -y gunicorn yq
+apt install -y python3-coverage python3-nose
