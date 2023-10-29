285bc9f146d7026189241fc6e8cb8111e0d63fa0
JonatanEnes <jonatan.enes@udc.es>
2023-10-29 21:39:38 +0100

    WIP

--- conf/CheckAlive/CheckAlive.py
+++ conf/CheckAlive/CheckAlive.py
@@ -3,7 +3,7 @@ from src.SanityChecker.CheckAliveBase import CheckAliveBase as CheckAlive
 
 
 def main():
-    hosts = ["host0", "host1"]
+    hosts = ["host0"]
     REST_SERVICES = [("{0}".format(h), h, "8000") for h in hosts]
     checker = CheckAlive()
     checker.set_REST_services(REST_SERVICES)
