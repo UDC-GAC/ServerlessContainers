6921c68aed3e8caeca2cbd1ee7ccabc6d8fb5d90
TomeMD <tome.maseda@udc.es>
2024-04-22 14:44:56 +0200

    [WattWizard] Remove unused and change timestamps location

--- conf/WattWizard/load-conf.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-confDir=$(dirname -- "$(readlink -f -- "$BASH_SOURCE")")
-
-export TRAIN_FILE_NAME=train
-export TRAIN_FILE=${confDir}/${TRAIN_FILE_NAME}.timestamps
-export MODEL_VARIABLES="user_load,system_load"
-export PREDICTION_METHODS="polyreg,sgdregressor"
-export INFLUXDB_BUCKET="compute2"
