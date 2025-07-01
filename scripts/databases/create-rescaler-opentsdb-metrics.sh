a02dc2f7cec6bdf09754c4905a3675ec095e4781
TomeMD <tome.maseda@udc.es>
2025-07-01 13:40:09 +0200

    [Rescaler] Optimise script to create OpenTSDB metrics (use a single Java process)

--- scripts/databases/create-rescaler-opentsdb-metrics.sh
+++ scripts/databases/create-rescaler-opentsdb-metrics.sh
@@ -1,76 +1,82 @@
 #!/usr/bin/env bash
 
-# LIMITS
-    ./build/tsdb mkmetric limit.cpu.lower
-    ./build/tsdb mkmetric limit.cpu.upper
-    ./build/tsdb mkmetric limit.cpu.boundary
-    ./build/tsdb mkmetric limit.mem.lower
-    ./build/tsdb mkmetric limit.mem.upper
-    ./build/tsdb mkmetric limit.mem.boundary
-    ./build/tsdb mkmetric limit.disk_read.lower
-    ./build/tsdb mkmetric limit.disk_read.upper
-    ./build/tsdb mkmetric limit.disk_read.boundary
-    ./build/tsdb mkmetric limit.disk_write.lower
-    ./build/tsdb mkmetric limit.disk_write.upper
-    ./build/tsdb mkmetric limit.disk_write.boundary
+METRICS=(
+  # LIMITS
+  # - CPU
+  limit.cpu.lower
+  limit.cpu.upper
+  limit.cpu.boundary
+  # - MEM
+  limit.mem.lower
+  limit.mem.upper
+  limit.mem.boundary
+  # - DISK
+  #   * READS
+  limit.disk_read.lower
+  limit.disk_read.upper
+  limit.disk_read.boundary
+  #   * WRITES
+  limit.disk_write.lower
+  limit.disk_write.upper
+  limit.disk_write.boundary
+  # - NET
+  limit.net.lower
+  limit.net.upper
+  limit.net.boundary
+  # - ENERGY
+  limit.energy.upper
+  limit.energy.lower
+  # RESOURCES
+  # - CPU
+  structure.cpu.usage
+  structure.cpu.current
+  structure.cpu.fixed
+  structure.cpu.max
+  structure.cpu.min
+  # - MEM
+  structure.mem.usage
+  structure.mem.current
+  structure.mem.fixed
+  structure.mem.max
+  structure.mem.min
+  # - DISK
+  #structure.disk.usage
+  structure.disk.current
+  #   * READS
+  structure.disk_read.usage
+  structure.disk_read.current
+  structure.disk_read.fixed
+  structure.disk_read.max
+  structure.disk_read.min
+  #   * WRITES
+  structure.disk_write.usage
+  structure.disk_write.current
+  structure.disk_write.fixed
+  structure.disk_write.max
+  structure.disk_write.min
+  # - NET
+  structure.net.usage
+  structure.net.current
+  structure.net.fixed
+  structure.net.max
+  structure.net.min
+  # - ENERGY
+  structure.energy.usage
+  structure.energy.current
+  structure.energy.shares
+  structure.energy.max
+  structure.energy.min
+  # SERVICES CONFIG
+  conf.guardian.window_delay
+  conf.guardian.event_timeout
+  conf.guardian.window_timelapse
+  conf.scaler.request_timeout
+  conf.scaler.polling_frequency
+  # USER
+  user.energy.used
+  user.energy.max
+  user.cpu.current
+  user.cpu.usage
+)
 
-    ./build/tsdb mkmetric limit.net.lower
-    ./build/tsdb mkmetric limit.net.upper
-    ./build/tsdb mkmetric limit.net.boundary
-    ./build/tsdb mkmetric limit.energy.upper
-    ./build/tsdb mkmetric limit.energy.lower
-
-# RESOURCES
-    ./build/tsdb mkmetric structure.cpu.usage
-    ./build/tsdb mkmetric structure.cpu.current
-    ./build/tsdb mkmetric structure.cpu.fixed
-    ./build/tsdb mkmetric structure.cpu.max
-    ./build/tsdb mkmetric structure.cpu.min
-
-    ./build/tsdb mkmetric structure.mem.usage
-    ./build/tsdb mkmetric structure.mem.current
-    ./build/tsdb mkmetric structure.mem.fixed
-    ./build/tsdb mkmetric structure.mem.max
-    ./build/tsdb mkmetric structure.mem.min
-
-    # ./build/tsdb mkmetric structure.disk.usage
-    ./build/tsdb mkmetric structure.disk.current
-
-    ./build/tsdb mkmetric structure.disk_read.usage
-    ./build/tsdb mkmetric structure.disk_read.current
-    ./build/tsdb mkmetric structure.disk_read.fixed
-    ./build/tsdb mkmetric structure.disk_read.max
-    ./build/tsdb mkmetric structure.disk_read.min
-
-    ./build/tsdb mkmetric structure.disk_write.usage
-    ./build/tsdb mkmetric structure.disk_write.current
-    ./build/tsdb mkmetric structure.disk_write.fixed
-    ./build/tsdb mkmetric structure.disk_write.max
-    ./build/tsdb mkmetric structure.disk_write.min
-
-
-    ./build/tsdb mkmetric structure.net.usage
-    ./build/tsdb mkmetric structure.net.current
-    ./build/tsdb mkmetric structure.net.fixed
-    ./build/tsdb mkmetric structure.net.max
-    ./build/tsdb mkmetric structure.net.min
-
-    ./build/tsdb mkmetric structure.energy.usage
-    ./build/tsdb mkmetric structure.energy.current
-    ./build/tsdb mkmetric structure.energy.shares
-    ./build/tsdb mkmetric structure.energy.max
-    ./build/tsdb mkmetric structure.energy.min
-
-# CONFIG
-    ./build/tsdb mkmetric conf.guardian.window_delay
-    ./build/tsdb mkmetric conf.guardian.event_timeout
-    ./build/tsdb mkmetric conf.guardian.window_timelapse
-    ./build/tsdb mkmetric conf.scaler.request_timeout
-    ./build/tsdb mkmetric conf.scaler.polling_frequency
-
-
-# USER
-    ./build/tsdb mkmetric user.energy.used
-    ./build/tsdb mkmetric user.energy.max
-    ./build/tsdb mkmetric user.cpu.current
-    ./build/tsdb mkmetric user.cpu.usage
+./build/tsdb mkmetric "${METRICS[@]}"
