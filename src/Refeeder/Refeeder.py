c26d4138d1ff7a943304f27ee8e11439df25fb9b
TomeMD <tome.maseda@udc.es>
2025-08-22 12:18:38 +0200

    [Refeeder] Added support to refeed users

--- src/Refeeder/Refeeder.py
+++ src/Refeeder/Refeeder.py
@@ -35,11 +35,8 @@ import logging
 import src.MyUtils.MyUtils as utils
 import src.StateDatabase.couchdb as couchdb
 import src.StateDatabase.opentsdb as bdwatchdog
-from src.ReBalancer.Utils import get_user_apps
 
-CONFIG_DEFAULT_VALUES = {"WINDOW_TIMELAPSE": 10, "WINDOW_DELAY": 20, "GENERATED_METRICS": ["cpu", "mem"], "DEBUG": True}
-
-host_info_cache = dict()
+CONFIG_DEFAULT_VALUES = {"WINDOW_TIMELAPSE": 10, "WINDOW_DELAY": 20, "STRUCTURES_REFEEDED": ["applications"], "GENERATED_METRICS": ["cpu", "mem"], "DEBUG": True}
 
 SERVICE_NAME = "refeeder"
 
@@ -51,7 +48,9 @@ class ReFeeder:
         self.opentsdb_handler = bdwatchdog.OpenTSDBServer()
         self.couchdb_handler = couchdb.CouchDBServer()
         self.NO_METRIC_DATA_DEFAULT_VALUE = self.opentsdb_handler.NO_METRIC_DATA_DEFAULT_VALUE
-        self.window_timelapse, self.window_delay, self.generated_metrics, self.debug, self.active = None, None, None, None, None
+        self.app_tracker, self.user_tracker = [], []
+        self.window_timelapse, self.window_delay, self.structures_refeeded = None, None, None
+        self.generated_metrics, self.debug, self.active = None, None, None
         self.config = {}
 
     @staticmethod
@@ -61,27 +60,19 @@ class ReFeeder:
         return out_dict
 
     def generate_user_metrics(self, user, applications):
-        resource_field_map = [("cpu", "current"), ("cpu", "usage"), ("energy", "usage")]
+        # Get all applications belonging to user and aggregate usages
+        user_apps = [app for app in applications if app["name"] in user["clusters"]]
+        for resource in self.generated_metrics:
+            user.setdefault(resource, {})["usage"] = 0
+            for app in user_apps:
+                value = app.get("resources", {}).get(resource, {}).get("usage", None)
+                if value is None:
+                    utils.log_warning("Missing usage for resource '{0}' in application {1} from user {2}".format(
+                        resource, app["name"], user["name"]), self.debug)
+                    continue
+                user[resource]["usage"] += value
 
-        # Initialize user values to zero
-        user.setdefault("cpu", {})
-        user.setdefault("energy", {})
-        total_user = {"cpu_current": 0, "cpu_usage": 0, "energy_usage": 0}
-
-        # Get all applications belonging to user and aggregate metrics
-        for app in get_user_apps(applications, user):
-            for resource, field in resource_field_map:
-                if app.get("resources", {}).get(resource, {}).get(field, None):
-                    total_user[f"{resource}_{field}"] += app["resources"][resource][field]
-                else:
-                    utils.log_error("Missing field '{0}' for resource '{1}' in application {2} from user {3}".format(
-                        field, resource, app["name"], user["name"]), self.debug)
-
-        # Update user values
-        for resource, field in resource_field_map:
-            user[resource][field] = total_user[f"{resource}_{field}"]
-
-        return user
+        self.user_tracker.append(user)
 
     def generate_application_metrics(self, application):
         application_info = dict()
@@ -101,34 +92,39 @@ class ReFeeder:
         if application_info: # check that application_info has been loaded with the information of at least one container
             for resource in self.generated_metrics:
                 if resource in application["resources"]:
-                    application["resources"][resource]["usage"] = application_info[utils.res_to_metric(resource)]
+                    if resource == "disk":  # Generate aggregated I/O usage
+                        application["resources"][resource]["usage"] = application_info.get(utils.res_to_metric("disk_read"), 0) + application_info.get(utils.res_to_metric("disk_write"), 0)
+                    else:
+                        application["resources"][resource]["usage"] = application_info[utils.res_to_metric(resource)]
                 else:
                     utils.log_warning("No resource {0} info for application {1}".format(resource, application["name"]), self.debug)
 
-        ## Generate aggregated I/O usage
-        if "disk" in application["resources"] and utils.res_to_metric("disk_read") in application_info and utils.res_to_metric("disk_write") in application_info:
-            application["resources"]["disk"]["usage"] = application_info[utils.res_to_metric("disk_read")] + application_info[utils.res_to_metric("disk_write")]
-
-        return application
-
-    def refeed_users(self, users, applications):
-        for user in users:
-            user = self.generate_user_metrics(user, applications)
-            utils.update_user(user, self.couchdb_handler, self.debug)
-
-    def refeed_applications(self, applications):
-        for application in applications:
-            application = self.generate_application_metrics(application)
-            utils.update_structure(application, self.couchdb_handler, self.debug)
+        self.app_tracker.append(application)
 
     def refeed_thread(self, ):
-        applications = utils.get_structures(self.couchdb_handler, self.debug, subtype="application")
-        if applications:
-            self.refeed_applications(applications)
+        applications = None
 
-        # users = db_handler.get_users()
-        # if users:
-        #     self.refeed_users(users, applications)
+        ts = time.time()
+        if "applications" in self.structures_refeeded:
+            applications = utils.get_structures(self.couchdb_handler, self.debug, subtype="application")
+            if applications:
+                utils.run_in_threads(applications, self.generate_application_metrics)
+        utils.log_info("It took {0} seconds to refeed applications".format(str("%.2f" % (time.time() - ts))), self.debug)
+
+        ts = time.time()
+        if "users" in self.structures_refeeded:
+            if not applications:
+                applications = utils.get_structures(self.couchdb_handler, self.debug, subtype="application")
+            users = self.couchdb_handler.get_users()
+            if users:
+                utils.run_in_threads(users, self.generate_user_metrics, applications)
+        utils.log_info("It took {0} seconds to refeed users".format(str("%.2f" % (time.time() - ts))), self.debug)
+
+        ts = time.time()
+        utils.run_in_threads(self.app_tracker + self.user_tracker, utils.persist_data, self.couchdb_handler, self.debug)
+        self.app_tracker.clear()
+        self.user_tracker.clear()
+        utils.log_info("It took {0} seconds to persist refeeded data in StateDatabase".format(str("%.2f" % (time.time() - ts))), self.debug)
 
     def refeed(self, ):
         myConfig = utils.MyConfig(CONFIG_DEFAULT_VALUES)
@@ -144,17 +140,17 @@ class ReFeeder:
 
             thread = None
             if self.active:
-                # Remote database operation
-                host_info_cache = dict()
-                thread = Thread(target=self.refeed_thread, args=())
-                thread.start()
+                if self.structures_refeeded:
+                    thread = Thread(target=self.refeed_thread, args=())
+                    thread.start()
+                else:
+                    utils.log_warning("No structures to refeed, check STRUCTURES_REFEEDED", self.debug)
             else:
                 utils.log_warning("Refeeder is not activated", self.debug)
 
             time.sleep(self.window_timelapse)
 
             utils.wait_operation_thread(thread, self.debug)
-            utils.log_info("Refeed processed", self.debug)
 
             utils.end_epoch(self.debug, self.window_timelapse, t0)
 
