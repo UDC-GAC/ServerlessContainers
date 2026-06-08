9b2235df4924295736a30b3b220ce02ef58c7a0e
TomeMD <tome.maseda@udc.es>
2026-06-08 11:07:03 +0200

    [Orchestrator] Save logs

--- scripts/services/orchestrator/start.sh
+++ scripts/services/orchestrator/start.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 scriptDir=$(dirname -- "$(readlink -f -- "$BASH_SOURCE")")
 source "${scriptDir}/../../../set_pythonpath.sh"
-python3 ${SERVERLESS_PATH}/src/Orchestrator/Orchestrator.py #2> orchestrator.log
+python3 ${SERVERLESS_PATH}/src/Orchestrator/Orchestrator.py | tee -a orchestrator.log
